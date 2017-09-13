73b3d0c45e4fc5df196fa145533d9c17878ea3df
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-13 14:23:00 -0400

    Updated from Brython Server: 9/13/2017 2:23:01 PM

--- unitConverter.py
+++ unitConverter.py
@@ -2,4 +2,11 @@
 #2017-09-13
 #unitCoverter.py - 
 
+num = int(input('Enter a number: '))
+if num== 1 and num ==2 and num ==3 and num == 4:
+    print(num)
+else:
+    print('Error, number must be integer between 1-4')
+degrees = float(input('Enter degrees in Celsius: '))
+print(degrees,'degrees Celsius is', ((degrees*1.8) + 32),'degrees in Fahrenheit')
 
