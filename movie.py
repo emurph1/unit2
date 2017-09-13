2feafd22ce954c9045b0fbc9e3b0e2ce87febde4
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-13 13:51:47 -0400

    Updated from Brython Server: 9/13/2017 1:51:49 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,14 @@
+#Emily Murphy
+#2017-09-13
+#movie.py - input age and print out most scandalous type of movie they can watch
+
+age = int(input('Enter age: '))
+if age>18:
+    print('You can watch rated R movies')
+if age<18 and age>13:
+    print('You can watch PG-13 movies')
+if age<13 and age>8:
+    print('You can watch PG movies')
+if age<8:
+    print('You can watch G movies')
 
