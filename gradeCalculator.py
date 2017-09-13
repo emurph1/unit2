582045bb83cc8a635c20da77a214ba7c7068351d
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-13 13:46:50 -0400

    Updated from Brython Server: 9/13/2017 1:46:51 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,15 @@
+#Emily Murphy
+#2017-09-13
+#gradeCalculator.py - input grade percentage and outputs letter grade
 
+grade = int(input('Enter grade percentage: '))
+if grade>0 and grade<59:
+    print('You earned a F')
+if grade>60 and grade<69:
+    print('You earned a D')
+if grade>70 and grade<79:
+    print('You earned a C')
+if grade>80 and grade<89:
+    print('You earned a B')
+if grade>90 and grade<100:
+    print('You earned a A')
