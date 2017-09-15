7ae1d51b7c16e2c2b8d30222389d4c9a7435789a
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-15 14:43:54 -0400

    Updated from Brython Server: 9/15/2017 2:43:55 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,22 @@
+#Emily Murphy
+#2017-09-15
+#fortuneTeller.py - pick color then number then fortune
 
+color = input('Pick red or blue: ')
+num = int(input('Pick a number from 1-4: '))
+if color == 'red' and num == 1:
+    print('You will become a billionaire.')
+if color == 'red' and num == 2:
+    print('You will die today.')
+if color == 'red' and num == 3:
+    print('You will live forever')
+if color == 'red' and num == 4:
+    print('You will fail school')
+if color == 'blue' and num == 1:
+    print('You will lose everything.')
+if color == 'blue' and num == 2:
+    print('You will find love.')
+if color == 'blue' and num == 3:
+    print('You will be attacked by a bear.')
+if color == 'blue' and num == 4:
+    print('You will live a happy life.')
