61a99ad40d9a5cb26631fb2eb76dd639e53815be
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-15 14:53:17 -0400

    Update insulter.py

--- insulter.py
+++ insulter.py
@@ -7,13 +7,13 @@ name = input('Enter name: ')
 insult = randint(1,5)
 if insult == 1:
     print('You are a nerd')
-if insult == 2:
+elif insult == 2:
     print('You are the worst person ever')
-if insult == 3:
+elif insult == 3:
     print('You are not funny')
-if insult == 4:
+elif insult == 4:
     print('I bet your sibling or parents are better than you')
-if insult == 5:
+elif insult == 5:
     print('You are probably an awful athlete/person')
 
 
